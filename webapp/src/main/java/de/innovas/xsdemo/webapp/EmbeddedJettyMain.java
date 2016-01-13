9c40c8954df9524fa0686c93d2772b64d70eace1
andre <bla>
2016-01-13 21:53:22 +0100

    maven repo initialisiert

---
+++ webapp/src/main/java/de/innovas/xsdemo/webapp/EmbeddedJettyMain.java
@@ -0,0 +1,64 @@
+package de.innovas.xsdemo.webapp;
+
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class EmbeddedJettyMain {
+
+	public static void main(String[] args) throws Exception {
+
+		Server server = new Server();
+
+		// Creating the web application context
+		WebAppContext webapp = new WebAppContext();
+		String webappDirLocation = "src/main/webapp";
+		webapp.setResourceBase(webappDirLocation);
+		webapp.setDescriptor(webappDirLocation + "/WEB-INF/web.xml");
+		webapp.setContextPath("/");
+		server.setHandler(webapp);
+
+		// HTTP Configuration
+		//HttpConfiguration http = new HttpConfiguration();
+		//http.addCustomizer(new SecureRequestCustomizer());
+
+		// Configuration for HTTPS redirect
+		//http.setSecurePort(8443);
+		//http.setSecureScheme("https");
+		ServerConnector connector = new ServerConnector(server);
+		//connector.addConnectionFactory(new HttpConnectionFactory(http));
+
+		// Setting HTTP port
+		connector.setPort(8080);
+		
+		// HTTPS configuration
+		//HttpConfiguration https = new HttpConfiguration();
+		//https.addCustomizer(new SecureRequestCustomizer());
+
+		// Configuring SSL
+		//SslContextFactory sslContextFactory = new SslContextFactory();
+
+		// Defining keystore path and passwords
+		//sslContextFactory.setKeyStorePath(EmbeddedJettyMain.class.getResource("keystore").toExternalForm());
+		//sslContextFactory.setKeyStorePassword("javacodegeeks");
+		//sslContextFactory.setKeyManagerPassword("javacodegeeks");
+
+		// Configuring the connector
+		//ServerConnector sslConnector = new ServerConnector(server,
+		//		new SslConnectionFactory(sslContextFactory, "http/1.1"), new HttpConnectionFactory(https));
+		//sslConnector.setPort(8443);
+
+		// Setting HTTP and HTTPS connectors
+		server.setConnectors(new Connector[] { connector });
+
+		// Starting the Server
+		server.start();
+		server.join();
+	}
+}
