c90966fd445159d4adef77228949d983fa338b4c
André Ehlert <aeh@innovas.de>
2016-01-18 18:01:57 +0100

    new xss script

--- app/src/main/java/de/innovas/xsdemo/security/CustomUserDetailsService.java
+++ app/src/main/java/de/innovas/xsdemo/security/CustomUserDetailsService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,48 +17,44 @@ import org.springframework.stereotype.Service;
 @Service("customUserDetailsService")
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private UserService userService;
-	static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-	
-	public CustomUserDetailsService() {
-		this.userService = new UserService();
-	}
+  @Autowired
+  private UserService userService;
+  static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-	@Override
-	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+  @Override
+  public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
 
-		Map<String, Object> userMap = userService.getUserByUsername(s);
+    Map<String, Object> userMap = userService.getUserByUsername(s);
 
-		if (userMap == null) {
-			throw new RuntimeException();
-			//throw new UsernameNotFoundException("User details not found with this username: " + s);
-		}
+    if (userMap == null) {
+      throw new UsernameNotFoundException( "User details not found with this username: " + s );
+    }
 
-		String username = (String) userMap.get("username");
-		String password = (String) userMap.get("password");
-		String role = (String) userMap.get("role");
+    String username = (String) userMap.get("username");
+    String password = (String) userMap.get("password");
+    String role = (String) userMap.get("role");
 
-		List<SimpleGrantedAuthority> authList = getAuthorities(role);
+    List<SimpleGrantedAuthority> authList = getAuthorities(role);
 
-		// get the encoded password
-		String encodedPassword = passwordEncoder.encode(password);
-		
-		User user = new User(username, encodedPassword, authList);
+    // get the encoded password
+    String encodedPassword = passwordEncoder.encode(password);
 
-		return user;
-	}
+    User user = new User(username, encodedPassword, authList);
 
-	private List<SimpleGrantedAuthority> getAuthorities(String role) {
-		List<SimpleGrantedAuthority> authList = new ArrayList<>();
-		authList.add(new SimpleGrantedAuthority("ROLE_USER"));
+    return user;
+  }
 
-		if (role != null && role.trim().length() > 0) {
-			if (role.equals("admin")) {
-				authList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-			}
-		}
+  private List<SimpleGrantedAuthority> getAuthorities(String role) {
+    List<SimpleGrantedAuthority> authList = new ArrayList<>();
+    authList.add(new SimpleGrantedAuthority("ROLE_USER"));
 
-		return authList;
-	}
-	
+    if (role != null && role.trim().length() > 0) {
+      if (role.equals("admin")) {
+        authList.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+      }
+    }
+
+    return authList;
+  }
+
 }
--- app/src/main/java/de/innovas/xsdemo/security/UserService.java
+++ app/src/main/java/de/innovas/xsdemo/security/UserService.java
@@ -9,17 +9,17 @@ import de.innovas.xsdemo.management.SignedUser;
 import de.innovas.xsdemo.management.User;
 
 @Service("userService")
-public class UserService {	
-	
-	public Map<String, Object> getUserByUsername(String username) {
-		Map<String, Object> userMap = null;
-		User user = SignedUser.getUser(username);
-		if (user != null) {
-			userMap = new HashMap<>();
-			userMap.put("username", user.getUsername());
-			userMap.put("password", user.getPassword());
-			userMap.put("role", (user.isVip() ? "admin" : "user"));
-		}
-		return userMap;
-	}
+public class UserService {
+
+  public Map< String, Object > getUserByUsername( String username ) {
+    Map<String, Object> userMap = null;
+    User user = SignedUser.getUser(username);
+    if (user != null) {
+      userMap = new HashMap<>();
+      userMap.put("username", user.getUsername());
+      userMap.put("password", user.getPassword());
+      userMap.put("role", (user.isVip() ? "admin" : "user"));
+    }
+    return userMap;
+  }
 }
